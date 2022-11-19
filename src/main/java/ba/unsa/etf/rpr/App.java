c0b904f49a06771f950a1bba48ebc2fe5e8d0661
ndzajic1 <ndzajic1@etf.unsa.ba>
2022-11-19 23:44:20 +0100

    Main handled

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,6 +2,6 @@ package ba.unsa.etf.rpr;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(ExpressionEvaluator.evaluate("( 1 + sqrt ( 4 ) )"));
+        System.out.println(ExpressionEvaluator.evaluate(args[0]));
     }
 }
